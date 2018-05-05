d542562af110edd594c32512578b3b67fda9652a
Shaoshing <shaoshing@me.com>
2018-05-04 20:29:36 -0700

    Await/Async

--- campsites.js
+++ campsites.js
@@ -7,67 +7,68 @@ require('datejs');
 
 const WEEK_NAMES = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-function _searchCampsiteURL(url, cookiesJar = request.jar(), results = []) {
-    return request({url: url, jar: cookiesJar})
-        .then((response) => {
-            let result = cheerio.load(response.body);
+async function _searchCampsiteURL(url, cookiesJar = request.jar(), results = []) {
+    let response = await request({url: url, jar: cookiesJar});
+        result = cheerio.load(response.body);
 
-            result("#calendar tr").each((index, element) => {
-                let campsiteName = result(".siteListLabel", element).text();
+    result("#calendar tr").each((index, element) => {
+        let campsiteName = result(".siteListLabel", element).text();
 
-                if (campsiteName === "") { // this is a separator
-                    return;
-                }
+        if (campsiteName === "") { // this is a separator
+            return;
+        }
 
-                let availables = result(".status.a a", element);
+        let availables = result(".status.a a", element);
 
-                if (availables.length === 0) {
-                    return;
-                }
+        availables.each((index, element) => {
+            let reservationURL = 'http://www.recreation.gov' + result(element).attr('href'),
+                dateStr = reservationURL.match(/\d+\/\d+\/\d+/)[0],
+                date = new Date(dateStr),
+                weekName = WEEK_NAMES[date.getDay()];
 
-                availables.each((index, element) => {
-                    let reservationURL = 'http://www.recreation.gov' + result(element).attr('href'),
-                        dateStr = reservationURL.match(/\d+\/\d+\/\d+/)[0],
-                        date = new Date(dateStr),
-                        weekName = WEEK_NAMES[date.getDay()];
-
-                    console.info(`[_searchCampsiteURL] found ${date}`);
-                    results.push({
-                        reservationURL,
-                        date,
-                        dateStr: `${date.toLocaleDateString()} ${weekName}`
-                    });
-                });
+            console.info(`[_searchCampsiteURL] found ${date}`);
+            results.push({
+                reservationURL,
+                date,
+                dateStr: `${date.toLocaleDateString()} ${weekName}`
             });
-
-            let nextPage = result('#resultNext').attr('href');
-
-            if (nextPage) {
-                return _searchCampsiteURL('http://www.recreation.gov' + nextPage, cookiesJar, results);
-            } else {
-                return results;
-            }
         });
+    });
+
+    let nextPage = result('#resultNext').attr('href');
+
+    if (nextPage) {
+        return _searchCampsiteURL('http://www.recreation.gov' + nextPage, cookiesJar, results);
+    } else {
+        return results;
+    }
 }
 
-function _searchCampsite(campsiteID, startDate, endDate, availables = []) {
+async function _searchCampsite(campsiteID, startDate, endDate, availables = []) {
     let startDateStr = startDate.format("%m/%d/%Y"),
         url = `http://www.recreation.gov/campsiteCalendar.do?page=calendar&contractCode=NRSO&parkId=${campsiteID}&calarvdate=${startDateStr}&sitepage=true`;
 
     console.info(`[_searchCampsite] searching ${campsiteID} - ${startDate.format("%m/%d/%Y")} - ${url}`);
 
-    return _searchCampsiteURL(url)
-        .then((results) => {
-            availables.push(...results.filter(r => !r.date.isAfter(endDate) && !r.date.equals(endDate) ));
+    let results = await _searchCampsiteURL(url);
 
-            let nextStartDate = new Date(startDate).addDays(14);
+    availables.push(...results.filter(r => {
+        let wrongDate = r.date.isAfter(endDate) || r.date.equals(endDate);
 
-            if (!nextStartDate.isAfter(endDate)) {
-                return _searchCampsite(campsiteID, nextStartDate, endDate, availables);
-            } else {
-                return availables;
-            }
-        })
+        if (wrongDate) {
+            console.info(`[_searchCampsite] skip ${r.date} (wrong date)`);
+        }
+
+        return !wrongDate;
+    }));
+
+    let nextStartDate = new Date(startDate).addDays(14);
+
+    if (!nextStartDate.isAfter(endDate)) {
+        return _searchCampsite(campsiteID, nextStartDate, endDate, availables);
+    } else {
+        return availables;
+    }
 }
 
 exports.searchCampsites = function (campsiteIDs, startDate, endDate) {
